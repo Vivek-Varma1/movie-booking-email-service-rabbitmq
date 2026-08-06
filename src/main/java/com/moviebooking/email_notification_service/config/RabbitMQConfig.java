b91ae20ea184d5333333513429dfab688503752c
vivek <vivekvarmanizampanam@gmail.com>
2026-08-07 01:28:06 +0530

    bug fixes

--- src/main/java/com/moviebooking/email_notification_service/config/RabbitMQConfig.java
+++ src/main/java/com/moviebooking/email_notification_service/config/RabbitMQConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,5 +33,12 @@ public class RabbitMQConfig {
                 .bind(bookingQueue())
                 .to(bookingExchange())
                 .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED); // fixed
+        return converter;
     }
 }
